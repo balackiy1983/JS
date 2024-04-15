55b99f094b075aeb98ef1f29abbd83b64cb70517
Balackiy Vitaliy <balackiy1983@gmail.com>
2024-04-15 15:05:33 +0300

    Deleted files

--- a_project/js/fineList.js
+++ a_project/js/fineList.js
@@ -9,8 +9,5 @@ let DB = data.finesData;
 function searchFines(searchKey){
     return DB.filter((item) => /\d{3}/.test(searchKey)? item.номер === searchKey: item.тип === searchKey);
    
-    return [
-        {номер: '001', тип: 'Перевищення швидкості', сума: 100, дата: '2023-01-15'}
-    ];
 }
 
